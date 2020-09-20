10e26fbfd2feff80516a76e25932eb879b0e716d
linjunfu <linjunfu@youxin.com>
2020-09-20 13:10:36 +0800

    118-generate

---
+++ src/three/array/118-generate/example.js
@@ -0,0 +1,18 @@
+// 执行用时：72 ms, 在所有 JavaScript 提交中击败了82.44%的用户
+// 内存消耗：36.9 MB, 在所有 JavaScript 提交中击败了25.76%的用户
+let generate = function (numRows) {
+    if (numRows <= 0) return []
+    let res = []
+    for (let i = 0; i < numRows; i++) {
+        let inner = []
+        for (let j = 0; j <= i; j++) {
+            if (j > 0 && j < i) {
+                inner.push(res[i - 1][j - 1] + res[i - 1][j])
+            } else {
+                inner.push(1)
+            }
+        }
+        res.push(inner)        
+    }
+    return res
+}
