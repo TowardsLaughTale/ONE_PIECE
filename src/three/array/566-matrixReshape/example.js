9eff7d2d3bd4de9a745b79c2b0a2659a255cc346
linjunfu <linjunfu@youxin.com>
2020-09-24 18:29:31 +0800

    566-matrixReshape

---
+++ src/three/array/566-matrixReshape/example.js
@@ -0,0 +1,23 @@
+// 执行用时：104 ms, 在所有 JavaScript 提交中击败了90.16%的用户
+// 内存消耗：43.5 MB, 在所有 JavaScript 提交中击败了19.33%的用户
+let matrixReshape = function (nums, r,c) {
+    if (!nums || nums.length === 0 ) return []
+
+    let rows = nums.length, cols = nums[0].length
+    
+    r = r ? r : 1
+    c = c ? c : 1
+
+    if (rows * cols < r * c) return nums
+    let res = []
+
+    // 平铺数组
+    nums = nums.reduce((init, cur) => init.concat(cur))
+
+    // 根据最后需要变动的行数，进行遍历
+    // 每次遍历将截取需要改动的 列数
+    for (let i = 0; i < r; i++) {
+        res.push(nums.splice(0, c))
+    }
+    return res
+}
